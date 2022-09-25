165f35976746b7fd5776ea90bdfc4d289f0da455
Andrew-clouds <113791725+Andrew-clouds@users.noreply.github.com>
2022-09-25 20:31:30 +0300

    Add files via upload

---
+++ WEB-APP-2022/01/02/script.js
@@ -0,0 +1,42 @@
+// console.log('hello Node!');
+
+// var username = 'Billy';
+
+// function show_username() {
+//    alert(`Hello, ${username}!`);
+//    username = 'Janin';
+//    var number = 1000;
+// }
+
+
+// show_username();
+
+// console.log(username);
+// console.log(number);
+
+// var say_hello = () => {
+//    console.log('Hello');
+// }
+
+// say_hello();
+
+// setTimeout(say_hello, 2000);
+
+// setTimeout(() => {
+//    console.log('Hello');
+// }, 3000);
+
+// var show_adult = (age) => {
+//    console.log(`Your age is ${age}`);
+//    (age >= 18) ? console.log('You are adult') : console.log('You are not adult');
+//    console.log('');
+// }
+
+// show_adult(12);
+// show_adult(21);
+
+// var get_sum = (a, b) => a + b;
+
+// var result = get_sum(10,15);
+// console.log(result);
+
